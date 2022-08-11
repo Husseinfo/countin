f5a0205cf99a4bff831c223eabec08708f1e2ff9
Hussein Srour <husseinfo@mail.com>
2022-08-11 12:05:23 +0200

    save date as long

--- app/src/main/java/io/github/husseinfo/countin/AddItemActivity.kt
+++ app/src/main/java/io/github/husseinfo/countin/AddItemActivity.kt
@@ -15,23 +15,25 @@ import kotlinx.coroutines.launch
 import java.util.*
 
 class AddItemActivity : AppCompatActivity() {
-    private lateinit var date: String
+    private var date: Long = 0L
     private lateinit var datePicker: DatePicker
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
         datePicker = findViewById(R.id.dp_new_item_date)
         val c = Calendar.getInstance()
+        date = c.time.time
         val currentYear = c[Calendar.YEAR]
         val currentMonth = c[Calendar.MONTH]
         val currentDay = c[Calendar.DAY_OF_MONTH]
-        date = "${currentDay}_${currentMonth}_$currentDay"
         datePicker.init(
             currentYear,
             currentMonth,
             currentDay
         ) { _: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-            date = "${year}_${monthOfYear}_$dayOfMonth"
+            val calendar = Calendar.getInstance()
+            calendar.set(year, monthOfYear, dayOfMonth)
+            date = calendar.time.time
         }
 
         findViewById<View>(R.id.btn_dismiss).setOnClickListener { finish() }
--- app/src/main/java/io/github/husseinfo/countin/data/CountModel.kt
+++ app/src/main/java/io/github/husseinfo/countin/data/CountModel.kt
@@ -3,18 +3,22 @@ package io.github.husseinfo.countin.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "count")
 class CountModel(
     @field:ColumnInfo(name = "title") var title: String, @field:ColumnInfo(
         name = "date"
-    ) var date: String
+    ) var date: Long
 ) {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     var id = 0
+
     val years: String
-        get() = "1"
+        get() {
+            return "0"
+        }
     val months: String
         get() = "2"
     val days: String
@@ -22,5 +26,11 @@ class CountModel(
 
     override fun toString(): String {
         return "$date;$title"
+    }
+
+    fun getDiff() {
+        val now = Calendar.getInstance().time
+        val c = Calendar.getInstance()
+//        c.set(date.split("_")[0], date.split("_")[1], date.split("_")[2])
     }
 }
